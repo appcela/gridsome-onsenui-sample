8fb11a4c638712fc68a6f9f0d61b934c6e0da371
Ivan Chang <(none)>
2019-05-03 01:28:28 +0800

    added external Onsen UI library

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+// import onsen ui css
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+  // register onsen ui for client only (window is available)
+  if (typeof window !== 'undefined') {
+    const VueOnsen = require('vue-onsenui');
+    Vue.use(VueOnsen);
+  }
+
 }
