cde4e46e131102a1a93235087488a36561d6fffb
Ivan Chang <(none)>
2019-05-03 01:28:50 +0800

    added Onsen hello world code

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,20 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <ClientOnly>
+      <v-ons-page>
+        <v-ons-toolbar>
+          <div class="center">About</div>
+        </v-ons-toolbar>
+
+        <p style="text-align: center">
+          <v-ons-button @click="$ons.notification.alert('Hello World!')">
+            Click me!
+          </v-ons-button>
+        </p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      </v-ons-page>
+    </ClientOnly>
   </Layout>
 </template>
 
