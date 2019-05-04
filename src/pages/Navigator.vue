57bb1aa3ab3e89b92236ed2c4b2c602bcdc7442c
Ivan Chang <(none)>
2019-05-04 16:10:52 +0800

    added Navigator demo

---
+++ src/pages/Navigator.vue
@@ -0,0 +1,25 @@
+<template>
+    <ClientOnly>
+        <v-ons-navigator swipeable
+                         :page-stack="pageStack"
+                         @push-page="pageStack.push($event)"
+        ></v-ons-navigator>
+    </ClientOnly>
+</template>
+
+<script>
+    import Page1 from './Page1';
+
+    export default {
+        name: "Navigator",
+        data() {
+            return {
+                pageStack: [Page1]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Page1.vue
@@ -0,0 +1,32 @@
+<template>
+    <ClientOnly>
+        <v-ons-page>
+            <v-ons-toolbar>
+                <div class="center">Page 1</div>
+            </v-ons-toolbar>
+            <p style="text-align: center">
+                This is the first page
+                <v-ons-button @click="push">Push Page 2</v-ons-button>
+            </p>
+        </v-ons-page>
+    </ClientOnly>
+</template>
+
+<script>
+    import Page2 from './Page2';
+    import Blogs from "../components/Blogs";
+
+    export default {
+        name: "Page1",
+        components: {Page2, Blogs},
+        methods: {
+            push() {
+                this.$emit('push-page', Page2);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
