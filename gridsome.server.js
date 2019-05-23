542e31e22abaa60e53443aa03295c43bd0e38d16
Ivan Chang <(none)>
2019-05-23 08:47:24 +0800

    added GraphQL source SWAPI to demonstrate how to fetch data from GraphQL endpoint

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,8 +4,35 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const { setContext } = require('apollo-link-context');
+const { HttpLink } = require('apollo-link-http');
+const { introspectSchema, makeRemoteExecutableSchema } = require('graphql-tools');
+const fetch = require('node-fetch');
 
 module.exports = function (api) {
+  // import remote GraphQL schema into Gridsome
+  api.createSchema(async function(graphql) {
+    const http = new HttpLink({
+      uri: 'https://api.graphcms.com/simple/v1/swapi ', // SWAPI - The Star Wars API for GraphQL
+      fetch
+    });
+
+    const link = setContext((request, previousContext) => ({
+      headers: {
+        // no authorization for SWAPI
+        // Authorization: `Bearer JSmxi5ocIhjKGENvgK66TrlqoylAPy8ZeAckiyo-4txKkYhdKSfLlPXLnxoghZm3`
+      }
+    })).concat(http);
+
+    const schema = await introspectSchema(link);
+    const executableSchema = await makeRemoteExecutableSchema({
+      schema: schema,
+      link
+    });
+
+    return executableSchema;
+  });
+
   api.loadSource(store => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
   })
--- src/pages/Navigator/Page2.vue
+++ src/pages/Navigator/Page2.vue
@@ -8,9 +8,21 @@
                 <div class="center">Page 2</div>
             </v-ons-toolbar>
             <p style="text-align: center">This is the second page</p>
+            <v-ons-list>
+                <v-ons-list-item v-for="person in $static.allPersons" :key="person.id">{{person.name}}</v-ons-list-item>
+            </v-ons-list>
         </v-ons-page>
     </ClientOnly>
 </template>
+
+<static-query>
+    query {
+        allPersons {
+            id,
+            name
+        }
+    }
+</static-query>
 
 <script>
     export default {
